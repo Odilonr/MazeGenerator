af793afb2f1b0fb8d35e238dfc522589590009d7
Odilon <rwabukambao@gmail.com>
2024-06-09 00:00:17 -0500

    Added Cells functionality for Maze

--- window.py
+++ window.py
@@ -2,16 +2,31 @@ from tkinter import Tk, BOTH, Canvas
 
 def main():
     win = Window(800, 600)
-    win.draw(Line(Point(0,0),Point(100,234)),"red")
+
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
+
     win.wait_for_close()
  
 
 class Window:
     def __init__(self, height, width):
         self.root = Tk()
-        self.root.geometry(f"{width}x{height}")
         self.root.title("Maze Solver")
-        self.canvas = Canvas(master=self.root, background='black')
+        self.canvas = Canvas(master=self.root,height=height, width=width)
         self.canvas.pack()
         self.running = False
         self.root.protocol("WM_DELETE_WINDOW", self.close)
@@ -38,13 +53,49 @@ class Point:
 
 class Line:
     def __init__(self, point1, point2):
-        self.x1 = point1.x
-        self.y1 = point1.y
-        self.x2 = point2.x
-        self.y2 = point2.y
+        self.point1 = point1
+        self.point2 = point2
        
     def draw(self, canvas,fill_color):
-        canvas.create_line(self.x1, self.y1, self.x2, self.y2,fill=fill_color, width=2)
+        canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y,fill=fill_color, width=2)
+
+class Cell:
+    def __init__(self,window):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.x1 = None
+        self.x2 = None
+        self.y1 = None
+        self.y2 = None
+        self.window = window
+
+    def draw(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+        if self.has_left_wall:
+            line_left = Line(Point(x1, y1), Point(x1, y2))
+            self.window.draw(line_left, "black")
+
+        if self.has_right_wall:
+            line_right = Line(Point(x2, y1), Point(x2, y2))
+            self.window.draw(line_right, "black")
+        
+        if self.has_top_wall:
+            line_top = Line(Point(x1, y1), Point(x2, y1))
+            self.window.draw(line_top, "black")
+        
+        if self.has_bottom_wall:
+            line_bottom = Line(Point(x1, y2), Point(x2, y2))
+            self.window.draw(line_bottom, "black")
+        
+
+
+        
+
 
 
 
